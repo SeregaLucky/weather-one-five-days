607d13375c5cc320fcc81961739cda036452798c
Sergii Stryzg <strizgsergey@gmail.com>
2020-02-24 17:08:50 +0200

    add pritter

--- src/redux/global/globalReducers.ts
+++ src/redux/global/globalReducers.ts
@@ -1,17 +1,24 @@
-import { combineReducers } from "redux";
-import { createReducer } from "@reduxjs/toolkit";
-import weatherT from "../weather/weatherTypes";
+import { combineReducers } from 'redux';
+import { createReducer } from '@reduxjs/toolkit';
+import {
+  GET_WEATHER_ONE_START,
+  GET_WEATHER_FIVE_START,
+  GET_WEATHER_ONE_SECCESS,
+  GET_WEATHER_ONE_FAILURE,
+  GET_WEATHER_FIVE_SECCESS,
+  GET_WEATHER_FIVE_FAILURE,
+} from '../weather/weatherTypes';
 
 const loadingReducer = createReducer(false, {
-  [weatherT.GET_WEATHER_ONE_START]: (): boolean => true,
-  [weatherT.GET_WEATHER_FIVE_START]: (): boolean => true,
+  [GET_WEATHER_ONE_START]: (): boolean => true,
+  [GET_WEATHER_FIVE_START]: (): boolean => true,
 
-  [weatherT.GET_WEATHER_ONE_SECCESS]: (): boolean => false,
-  [weatherT.GET_WEATHER_ONE_FAILURE]: (): boolean => false,
-  [weatherT.GET_WEATHER_FIVE_SECCESS]: (): boolean => false,
-  [weatherT.GET_WEATHER_FIVE_FAILURE]: (): boolean => false
+  [GET_WEATHER_ONE_SECCESS]: (): boolean => false,
+  [GET_WEATHER_ONE_FAILURE]: (): boolean => false,
+  [GET_WEATHER_FIVE_SECCESS]: (): boolean => false,
+  [GET_WEATHER_FIVE_FAILURE]: (): boolean => false,
 });
 
 export default combineReducers({
-  loading: loadingReducer
+  loading: loadingReducer,
 });
