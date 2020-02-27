e4411daa56183d1f54e1003ab546b9f2dd0b4d99
Sergii Stryzg <strizgsergey@gmail.com>
2020-02-27 19:55:14 +0200

    add TS in api.js

--- src/servises/api.ts
+++ src/servises/api.ts
@@ -3,16 +3,29 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://api.openweathermap.org/data/2.5';
 const openWeatherApiKey = 'c792484ade42380886f51003cfcaf04d';
 
-const getOneDayWeather = async city => {
-  const res = await axios.get(
+interface IDataOneDay {
+  name: string;
+  main: { temp: number; feels_like: number };
+  clouds: { all: number };
+  weather: [
+    {
+      description: string;
+      icon: string;
+    },
+  ];
+  wind: { speed: number };
+}
+
+const getOneDayWeather = async (city: string) => {
+  const res = await axios.get<IDataOneDay>(
     `/weather?q=${city},ua&units=metric&mode=json&appid=${openWeatherApiKey}`,
   );
 
   return res.data;
 };
 
-const getFiveDayWeather = async city => {
-  const res = await axios.get(
+const getFiveDayWeather = async (city: string) => {
+  const res = await axios.get<object>(
     `/forecast?q=${city},ua&units=metric&mode=json&appid=${openWeatherApiKey}`,
   );
 
